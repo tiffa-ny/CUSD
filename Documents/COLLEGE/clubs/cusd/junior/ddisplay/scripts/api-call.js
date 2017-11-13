1e9a68dce3f2a5311fd295524a9d9ab9389ada9d
tiffa-ny <tw292@cornell.edu>
2017-11-13 14:36:20 -0500

    map refresh, infinite markers

--- Documents/COLLEGE/clubs/cusd/junior/ddisplay/scripts/api-call.js
+++ Documents/COLLEGE/clubs/cusd/junior/ddisplay/scripts/api-call.js
@@ -1,22 +1,26 @@
-
 $(document).ready(function () {
   var token = 'Bearer e5159b89-86c1-3cca-8412-59de037c674b';
-  $.ajax({
-    url: 'https://gateway.api.cloud.wso2.com:443/t/mystop/tcat/v1/rest/Vehicles/GetAllVehicles',
-    type: 'GET',
-    dataType: 'json',
-    beforeSend: function(xhr) {
-      xhr.setRequestHeader("Authorization", token);
-    },
-    //data: 'json=' + escape(JSON.stringify(createRequestObject)),
-    success: function(msg) {
-      var data = JSON.stringify(msg);
-      //console.log(msg);
-      MapRoute(msg);
-      //$("#orders").text(data);
-    },
-    error: function(XMLHttpRequest, textStatus, errorThrown) {
-      alert(errorThrown);
-    }
-  });
-});
+  var data;
+  (function update() {
+    $.ajax({
+      url: 'https://gateway.api.cloud.wso2.com:443/t/mystop/tcat/v1/rest/Vehicles/GetAllVehicles',
+      type: 'GET',
+      dataType: 'json',
+      async: false,
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader("Authorization", token);
+      },
+      //data: 'json=' + escape(JSON.stringify(createRequestObject)),
+      success: function(msg) {
+        data = msg;
+        MapRoute(msg);
+      },
+      error: function(XMLHttpRequest, textStatus, errorThrown) {
+        alert(errorThrown);
+      }
+    }).then(function() {
+      setTimeout(update,5000); //refreshes call every 5 seconds
+      //setMapOnAll(null);
+    });
+  })();
+  }); 
